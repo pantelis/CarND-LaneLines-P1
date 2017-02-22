60cd10e7d0ee782270e7498b4de353f24544fce0
Pantelis Monogioudis <monogioudis@yahoo.com>
2017-02-21 20:20:56 -0500

    further edits on image lane detection

--- photo-lane-detection-pipeline.py
+++ photo-lane-detection-pipeline.py
@@ -27,20 +27,10 @@ parameters.Hough.max_line_gap=10 # maximum gap in pixels between connectable lin
 imageSourceDir = "test_images/"
 imageTestDir = "test_images/"
 
-#params = helpers.DictQuery(parameter_dict[0])
-
 for i in os.listdir(imageSourceDir):
 
     # Make copies into the test_images directory
     image = mpimg.imread(os.path.join(imageSourceDir, i))
-
-    # print('This image is: ', type(image),
-    #       'with dimesions:', image.shape)
-    #
-    # # display original image
-    # plt.figure("Original Image")
-    # plt.imshow(image)
-    # plt.show(block=False)
 
     # Pull out the x and y sizes and make a copy of the image
     ysize = image.shape[0]
@@ -67,29 +57,11 @@ for i in os.listdir(imageSourceDir):
     # masking
     masked_edges = helpers.region_of_interest(edges, parameters.Masking.vertices)
 
-    # Define the Hough transform parameters
-    # Make a blank the same size as our image to draw on
-
-    line_image = np.copy(image) * 0  # creating a blank to draw lines on
-
     # Run Hough on edge detected image
-    # Output "lines" is an array containing endpoints of detected line segments
-    lines = cv2.HoughLinesP(masked_edges, parameters.Hough.rho, parameters.Hough.theta, parameters.Hough.threshold, np.array([]),
-                            parameters.Hough.min_line_length, parameters.Hough.max_line_gap)
+    lines = helpers.hough_lines(masked_edges, parameters.Hough.rho, parameters.Hough.theta, parameters.Hough.threshold,
+                             parameters.Hough.min_line_length, parameters.Hough.max_line_gap)
 
-    # Iterate over the output "lines" and draw lines on a blank image
-    for line in lines:
-        for x1, y1, x2, y2 in line:
-            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
-
-    # Create a "color" binary image to combine with line image
-    color_edges = np.dstack((edges, edges, edges))
-
-    # Draw the lines on the edge image
-    lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0)
-    plt.imshow(lines_edges)
-    plt.show()
-
+    superposed_image = helpers.weighted_img(lines, image, α=0.8, β=1., λ=0.)
     # then save them to the test_images directory.
     output_path_filename = os.path.join(imageSourceDir, "out_"+i)
-    cv2.imwrite(output_path_filename,lines_edges)
+    cv2.imwrite(output_path_filename,superposed_image)
